6090de298bcc64e322349acaaf0f28b03be3216a
Mr-Archisman <1807327@kiit.ac.in>
2024-06-26 20:23:58 +0530

    change axios

--- frontend/src/Axios/axios.js
+++ frontend/src/Axios/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 const instance = axios.create({
-    baseURL:"http://localhost:8000/api"
+    baseURL:"https://todo-multiuser-back.vercel.app/api"
 })
 export default instance
