3c1ca588fcda74575fcd10230829d63681fd24b0
Mr-Archisman <1807327@kiit.ac.in>
2024-06-26 20:28:52 +0530

    change cors

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,10 @@ const port = process.env.PORT || 8001
 mongoose.set('strictQuery', true);
 
 //middlewares
-app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: 'https://todo-multiuser.vercel.app/', // Update with your frontend URL
+    credentials: true
+  }));
 
 //db config
 mongoose.connect(process.env.MONGO_URI, {
