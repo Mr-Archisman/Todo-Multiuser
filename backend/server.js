98e9373ce584811c25b23a4c888cd2cee1790d32
Mr-Archisman <1807327@kiit.ac.in>
2024-06-27 19:17:24 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -11,14 +11,18 @@ import forgotPasswordRouter from "./routes/forgotPassword.js"
 dotenv.config()
 const app = express()
 const port = process.env.PORT || 8001
-mongoose.set('strictQuery', true);
+mongoose.set('strictQuery', false);
 
 //middlewares
+app.use(express.json())
+app.use(cors())
 app.use(cors({
-    origin: 'https://todo-multiuser.vercel.app', // Update with your frontend URL
+    origin: 'https://todo-multiuser-huv0lcpxk-mrarchismans-projects.vercel.app', // Replace with your actual frontend URL
     credentials: true
-  }));
-
+}));
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
 //db config
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
